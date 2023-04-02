50128b0297e140567b051dcdb9c5c6e7612df1da
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-02 18:36:24 +0530

    added browser router

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,15 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "../src/redux/store";
 
+import { BrowserRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
