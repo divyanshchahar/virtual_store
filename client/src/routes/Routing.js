8fd28bbdee7fe687f5305c607a92ee9be857dfba
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-05 14:57:15 +0530

    added route

--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import SingleProductPage from "../pages/SingleProductPage";
+import RegistrationPage from "../pages/RegistrationPage";
 
 function Routing() {
   return (
@@ -9,6 +10,7 @@ function Routing() {
         <Route path="/">
           <Route index element={<HomePage />} />
           <Route path="products/:productId" element={<SingleProductPage />} />
+          <Route path="user_registration" element={<RegistrationPage />} />
         </Route>
       </Routes>
     </>
