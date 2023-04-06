1ca615278fae81784be1add5acdfa6f859452905
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-06 18:10:57 +0530

    imported bootstrap-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 
 import Routing from "./routes/Routing";
 
