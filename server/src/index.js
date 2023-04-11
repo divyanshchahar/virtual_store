37f46030d01164b4413272d3a4dc45afa5c360b2
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-11 22:30:10 +0530

    integrated router

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const productsRouter = require("./routes/products");
+const userRouter = require("./routes/userRoute");
 
 const app = express();
 
@@ -9,4 +10,6 @@ mongoose.connect("mongodb://localhost/virtual_store");
 
 app.listen(3200);
 
+app.use(express.json());
 app.use("/products", productsRouter);
+app.use("/users", userRouter);
