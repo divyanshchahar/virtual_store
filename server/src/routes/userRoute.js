59112f41b7ee389ec4070092b8e73ee8723f0149
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-16 15:33:19 +0530

    added get method
    - resolved #23

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -2,11 +2,20 @@ const express = require("express");
 const User = require("../schema/userSchema");
 
 const checkJwt = require("../middleware/checkJwt");
-const { checkCartScopes } = require("../middleware/checkScopes");
+const { checkUsersScopes } = require("../middleware/checkScopes");
 
 const router = express.Router();
 
-router.route("/").post(checkJwt, checkCartScopes, async (req, res) => {
+router.route("/").get(checkJwt, checkUsersScopes, async (req, res) => {
+  try {
+    const user = await User.find({ email: req.body.email });
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.route("/").post(checkJwt, checkUsersScopes, async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(200).send(user._id);
@@ -15,7 +24,7 @@ router.route("/").post(checkJwt, checkCartScopes, async (req, res) => {
   }
 });
 
-router.route("/").put(checkJwt, checkCartScopes, async (req, res) => {
+router.route("/").put(checkJwt, checkUsersScopes, async (req, res) => {
   try {
     const user = await User.findById(req.body._id);
     Object.entries(req.body).forEach((item) => {
@@ -28,7 +37,7 @@ router.route("/").put(checkJwt, checkCartScopes, async (req, res) => {
   }
 });
 
-router.route("/:id").delete(checkJwt, checkCartScopes, async (req, res) => {
+router.route("/:id").delete(checkJwt, checkUsersScopes, async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     res.status(200).send("deleted");
