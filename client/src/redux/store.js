cd004349e0fb5a0f8b4ac4260717eb66acffb7ef
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-17 17:39:59 +0530

    added slice to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import productsReducer from "./productsSlice";
 import ordersReducer from "./ordersSlice";
+import usersSlice from "./usersSlice";
 
 const store = configureStore({
-  reducer: { products: productsReducer, orders: ordersReducer },
+  reducer: {
+    products: productsReducer,
+    orders: ordersReducer,
+    users: usersSlice,
+  },
 });
 
 export default store;
