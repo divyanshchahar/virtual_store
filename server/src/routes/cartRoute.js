7a0dbced7032fb797f208eaf3f622fc2553a6fc5
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-24 18:45:46 +0530

    updated cartRoute

--- server/src/routes/cartRoute.js
+++ server/src/routes/cartRoute.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.route("/").post(checkJwt, checkCartScopes, async (req, res) => {
   try {
     const cart = await Cart.create(req.body);
-    res.status(200).send(cart._id);
+    res.status(200).send(cart);
   } catch (e) {
     res.status(500).send(e.message);
   }
@@ -19,8 +19,9 @@ router.route("/").put(checkJwt, checkCartScopes, async (req, res) => {
   try {
     let itemNotPresent = true;
 
-    const cart = await Cart.findById(req.body._id);
+    const [cart] = await Cart.find({ customerId: req.body.customerId });
 
+    // updating product quantity
     cart.products = cart.products.map((item) => {
       if (item.productId == req.body.productId) {
         item.qty = req.body.qty;
@@ -30,16 +31,17 @@ router.route("/").put(checkJwt, checkCartScopes, async (req, res) => {
       return item;
     });
 
+    // pushing product if not present
     if (itemNotPresent) {
       cart.products.push({ productId: req.body.productId, qty: req.body.qty });
     }
 
+    // removing products
     cart.products = cart.products.filter((item) => item.qty > 0);
 
-    console.log(cart.products);
     await cart.save();
 
-    res.status(200).send("cart updated sucessfully");
+    res.status(200).send(cart);
   } catch (e) {
     res.status(500).send(e.message);
   }
