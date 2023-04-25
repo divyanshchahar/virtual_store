439f765afdf71cd69fd2e9d04900763e69d874f3
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-25 15:26:17 +0530

    integrated cartUpdateApi

--- client/src/layouts/ProductsList.js
+++ client/src/layouts/ProductsList.js
@@ -1,19 +1,166 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { fetchProducts } from "../redux/productsSlice";
 import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../redux/productsSlice";
+import { getUsers } from "../redux/usersSlice";
+import { getCartApi, createCartApi, updateCartApi } from "../redux/cartSlice";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function ProductsList() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { getAccessTokenSilently, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  const products = useSelector((state) => state.products);
+  const selectedUser = useSelector((state) => state.users.users);
+  const cart = useSelector((state) => state.cart.cart);
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    const setUserOnPageLoad = async () => {
+      try {
+        if (isAuthenticated && !isLoading) {
+          const acessToken = await getAccessTokenSilently({
+            authorizationParams: {
+              audience: process.env.REACT_APP_AUDIENCE,
+              scope: "write:users",
+            },
+          });
 
-  const products = useSelector((state) => state.products);
+          const authId = user.sub;
+          const data = { authId, acessToken };
 
-  const navigate = useNavigate();
+          dispatch(getUsers(data));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    setUserOnPageLoad();
+  }, [user]);
+
+  useEffect(() => {
+    const setCartOnPageLoad = async () => {
+      try {
+        if (selectedUser._id) {
+          const acessToken = await getAccessTokenSilently({
+            authorizationParams: {
+              audience: process.env.REACT_APP_AUDIENCE,
+              scope: "write:carts",
+            },
+          });
+
+          const customerId = selectedUser._id;
+          const data = { acessToken, customerId };
+
+          dispatch(getCartApi(data));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    setCartOnPageLoad();
+  }, [selectedUser]);
+
+  const addToCart = async (productId) => {
+    // user loggedin and registered
+    if (selectedUser._id) {
+      const acessToken = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: process.env.REACT_APP_AUDIENCE,
+          scope: "write:carts",
+        },
+      });
+
+      // if user has a cart
+      if (cart) {
+        const itemPresent = cart.products.filter(
+          (item) => item.productId === productId
+        );
+
+        // if item is availible in cart
+        if (itemPresent.length > 0) {
+          const data = {
+            acessToken: acessToken,
+            cartData: {
+              customerId: selectedUser._id,
+              productId: productId,
+              qty: itemPresent[0].qty + 1,
+            },
+          };
+
+          dispatch(updateCartApi(data));
+        } else {
+          // put request with qty:1
+
+          const data = {
+            acessToken: acessToken,
+            cartData: {
+              customerId: selectedUser._id,
+              productId: productId,
+              qty: 1,
+            },
+          };
+
+          dispatch(updateCartApi(data));
+        }
+      } else {
+        const data = {
+          acessToken: acessToken,
+          cartData: {
+            customerId: selectedUser._id,
+            productId: productId,
+            qty: 1,
+          },
+        };
+
+        dispatch(createCartApi(data));
+      }
+    } else {
+      alert("Please login and register user details");
+    }
+  };
+
+  const removeFromCart = async (productId) => {
+    if (selectedUser._id) {
+      if (cart) {
+        const [isProduct] = cart.products.filter(
+          (item) => item.productId === productId
+        );
+
+        if (isProduct) {
+          const acessToken = await getAccessTokenSilently({
+            authorizationParams: {
+              audience: process.env.REACT_APP_AUDIENCE,
+              scope: "write:carts",
+            },
+          });
+
+          const data = {
+            acessToken: acessToken,
+            cartData: {
+              customerId: selectedUser._id,
+              productId: productId,
+              qty: isProduct.qty - 1,
+            },
+          };
+          dispatch(updateCartApi(data));
+        } else {
+          alert("This product does not exists in cart");
+        }
+      } else {
+        alert("Opps! No cart exists");
+      }
+    } else {
+      alert("Please login and register user details");
+    }
+  };
 
   return (
     <>
@@ -65,10 +212,20 @@ function ProductsList() {
                   </p>
 
                   <div className="btn-group">
-                    <button className="btn btn-primary">
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        addToCart(item._id);
+                      }}
+                    >
                       <i class="bi bi-bag-plus"></i>
                     </button>
-                    <button className="btn btn-primary">
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        removeFromCart(item._id);
+                      }}
+                    >
                       <i class="bi bi-bag-dash"></i>
                     </button>
                   </div>
