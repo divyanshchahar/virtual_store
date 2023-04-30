fd66d9320598fb52f9dd6f7a0ea529205403ccf3
Divyansh Chahar <divyanshchahar@outlook.com>
2023-04-30 13:42:56 +0530

    debugged adresses

--- client/src/assets/api_endpoints.js
+++ client/src/assets/api_endpoints.js
@@ -1,8 +1,8 @@
 const apiEndPoints = {
-  products: `${process.env.REACT_APP_URL}/products`,
-  orders: `${process.env.REACT_APP_URL}/orders`,
-  users: `${process.env.REACT_APP_URL}/users`,
-  cart: `${process.env.REACT_APP_URL}/cart`,
+  products: `${process.env.REACT_APP_AUDIENCE}products`,
+  orders: `${process.env.REACT_APP_AUDIENCE}orders`,
+  users: `${process.env.REACT_APP_AUDIENCE}users`,
+  cart: `${process.env.REACT_APP_AUDIENCE}cart`,
 };
 
 export default apiEndPoints;
