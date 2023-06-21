96ffce57534c86c25b03e206421f53652990b15e
Divyansh Chahar <divyanshchahar@outlook.com>
2023-06-21 17:50:22 +0530

    added jwt security - added functionality to generate refresh and acess token

--- server/src/routes/signUpRoute.js
+++ server/src/routes/signUpRoute.js
@@ -2,17 +2,42 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../schema/userSchema");
 
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 
 router.route("/").post(async (req, res) => {
   try {
+    if (!req.body.password || !req.body.email) return res.sendStatus(400);
+
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const hashedUser = { ...req.body, password: hashedPassword };
+
     const user = await User.create(hashedUser);
 
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send(e.message);
+    const acessToken = jwt.sign(
+      { email: req.body.email },
+      process.env.ACESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { email: req.body.email },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .send({ user: user, acessToken: acessToken })
+      .status(200);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
