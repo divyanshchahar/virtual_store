7743ce06829a06badd400c2d2bc267a0544c6f5d
Divyansh Chahar <divyanshchahar@outlook.com>
2023-06-21 21:45:26 +0530

    removed middleware

--- server/src/routes/ordersRoute.js
+++ server/src/routes/ordersRoute.js
@@ -1,25 +1,19 @@
 const express = require("express");
 
-const checkJwt = require("../middleware/checkJwt");
-
-const { checkOrdersScopes } = require("../middleware/checkScopes");
-
 const Orders = require("../schema/ordersSchema");
 
 const router = express.Router();
 
-router
-  .route("/:customerId")
-  .get(checkJwt, checkOrdersScopes, async (req, res) => {
-    try {
-      const orders = await Orders.find({ customerId: req.params.customerId });
-      res.status(200).send(orders);
-    } catch (e) {
-      res.status(500).send(e.mesage);
-    }
-  });
+router.route("/:customerId").get(async (req, res) => {
+  try {
+    const orders = await Orders.find({ customerId: req.params.customerId });
+    res.status(200).send(orders);
+  } catch (e) {
+    res.status(500).send(e.mesage);
+  }
+});
 
-router.route("/").post(checkJwt, checkOrdersScopes, async (req, res) => {
+router.route("/").post(async (req, res) => {
   try {
     const orders = await Orders.create(req.body);
     res.status(200).send("order placed sucessfully");
