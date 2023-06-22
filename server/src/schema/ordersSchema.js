322efbac302b6ccca0a6afb1b917429686fc2fae
Divyansh Chahar <divyanshchahar@outlook.com>
2023-06-22 10:18:37 +0530

    changed orderSchema

--- server/src/schema/ordersSchema.js
+++ server/src/schema/ordersSchema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const cartItems = new mongoose.Schema({
-  productId: mongoose.SchemaTypes.ObjectId,
-  qty: { type: Number, min: 0 },
-});
-
 const ordersSchema = new mongoose.Schema({
   customerId: mongoose.SchemaTypes.ObjectId,
-  products: [cartItems],
+  products: [
+    {
+      productId: { type: mongoose.SchemaTypes.ObjectId, required: true },
+      qty: { type: Number, min: 0, required: true },
+    },
+  ],
   createdAt: {
     type: Date,
     default: () => Date.now(),
