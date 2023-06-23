85e0078b149a8a6512c34ebbf5ffe63055f1365a
Divyansh Chahar <divyanshchahar@outlook.com>
2023-06-23 19:00:30 +0530

    debugged middleware

--- server/src/middleware/authorizationMiddleware.js
+++ server/src/middleware/authorizationMiddleware.js
@@ -1,18 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 function authorizeAcess(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+    if (!token) {
+      return res.sendStatus(401);
+    }
 
-  jwt.verify(token, process.env.ACESS_TOKEN_SECRET, (error, decoded) => {
-    if (error) return res.sendStatus(403);
+    jwt.verify(token, process.env.ACESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        res.sendStatus(403);
+        return;
+      }
+      req.id = decoded.id;
 
-    req.id = decoded.id;
-  });
-
-  next();
+      next();
+    });
+  } catch (error) {
+    res.send(error.message).status(500);
+  }
 }
 
 module.exports = authorizeAcess;
