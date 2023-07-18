416061d73faa5430153b5ee98cafb41a8f2ebb98
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-18 16:01:44 +0530

    added custom hook

---
+++ client/src/hooks/useMakeAuthRequest.js
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContextProvider";
+import { useDispatch } from "react-redux";
+
+function useMakeAuthRequest() {
+  const { auth, refreshAuth, lastUpdated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+
+  return async (state, reducer, body = null) => {
+    try {
+      const tokenAge = (Date.now() - lastUpdated) / (1000 * 60);
+
+      if (
+        state.responseCode === 403 ||
+        !auth ||
+        tokenAge > 13 ||
+        !tokenAge ||
+        !lastUpdated
+      ) {
+        console.log(tokenAge);
+        const newAuth = await refreshAuth();
+
+        if (body) {
+          dispatch(reducer(newAuth, body));
+        } else {
+          dispatch(reducer(newAuth));
+        }
+      } else {
+        if (body) {
+          dispatch(reducer(auth, body));
+        } else {
+          dispatch(reducer(auth));
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
+
+export default useMakeAuthRequest;
