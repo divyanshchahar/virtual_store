ff3570bb4f342576e77b832a0ec598b9b61158c8
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-20 12:53:19 +0530

    added side effects - added logic to update acess token on page load

--- client/src/pages/RootPage.js
+++ client/src/pages/RootPage.js
@@ -1,7 +1,19 @@
 import { Outlet } from "react-router-dom";
 import TopNavBar from "../layouts/TopNavBar";
+import { useEffect, useContext } from "react";
+import AuthContext from "../context/AuthContextProvider";
 
 function RootPage() {
+  const { refreshAuth, isLoggedIn } = useContext(AuthContext);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      makeRefreshRequest();
+    }
+  }, []);
+
+  const makeRefreshRequest = async () => {
+    await refreshAuth();
+  };
   return (
     <>
       <TopNavBar />
