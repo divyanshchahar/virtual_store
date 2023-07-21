794c6bf2987f078442109e6a7f6c8cc65f0ab5c0
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-21 22:57:15 +0530

    added route protector

--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -5,6 +5,9 @@ import AccountPage from "../pages/AccountPage";
 import RootPage from "../pages/RootPage";
 import CartPage from "../pages/CartPage";
 import OrdersPage from "../pages/OrdersPage";
+import RouteProtector from "./RouteProtector";
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
 
 function Routing() {
   return (
@@ -13,9 +16,32 @@ function Routing() {
         <Route path="/" element={<RootPage />}>
           <Route index element={<ProductsListPage />} />
           <Route path="products/:productId" element={<SingleProductPage />} />
-          <Route path="account" element={<AccountPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="orders" element={<OrdersPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route
+            path="account"
+            element={
+              <RouteProtector>
+                <AccountPage />
+              </RouteProtector>
+            }
+          />
+          <Route
+            path="cart"
+            element={
+              <RouteProtector>
+                <CartPage />
+              </RouteProtector>
+            }
+          />
+          <Route
+            path="orders"
+            element={
+              <RouteProtector>
+                <OrdersPage />
+              </RouteProtector>
+            }
+          />
         </Route>
       </Routes>
     </>
