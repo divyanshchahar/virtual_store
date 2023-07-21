dfbc1e9992a90aeff03f88c63796502780e21ea2
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-21 22:23:36 +0530

    updated put request

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -25,27 +25,36 @@ router.route("/").put(authorizationMiddleware, async (req, res) => {
 
     if (!user) return res.sendStatus(404);
 
-    user.name = null || req.body.name;
-    user.email = null || req.body.email;
-    user.password = null || (await bcrypt.hash(req.body.password, 10));
+    const isAuthenticatd = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
 
-    user.address.house = null || req.body.address.house;
-    user.address.street = null || req.body.address.street;
-    user.address.city = null || req.body.address.city;
-    user.address.pin = null || req.body.address.pin;
-    user.address.country = null || req.body.address.country;
+    if (isAuthenticatd) {
+      user.name = null || req.body.name;
+      user.email = null || req.body.email;
+      user.password = null || (await bcrypt.hash(req.body.password, 10));
 
-    user.payments.nameOnCard = null || req.body.payments.nameOnCard;
-    user.payments.cardNo = null || req.body.payments.cardNo;
-    user.payments.validFrom = null || req.body.payments.validFrom;
-    user.payments.validUpto = null || req.body.payments.validUpto;
-    user.payments.cvv = null || req.body.payments.cvv;
+      user.address.house = null || req.body.address.house;
+      user.address.street = null || req.body.address.street;
+      user.address.city = null || req.body.address.city;
+      user.address.pin = null || req.body.address.pin;
+      user.address.country = null || req.body.address.country;
 
-    user.save();
+      user.payments.nameOnCard = null || req.body.payments.nameOnCard;
+      user.payments.cardNo = null || req.body.payments.cardNo;
+      user.payments.validFrom = null || req.body.payments.validFrom;
+      user.payments.validUpto = null || req.body.payments.validUpto;
+      user.payments.cvv = null || req.body.payments.cvv;
 
-    res.sendStatus(200);
+      const updatedUser = await user.save();
+
+      return res.send(updatedUser).status(200);
+    }
+
+    return res.send(user).status(401);
   } catch (error) {
-    res.sendStatus(500).send(error.message);
+    return res.status(500).send(error.message);
   }
 });
 
@@ -56,7 +65,8 @@ router.route("/").delete(authorizationMiddleware, async (req, res) => {
     if (!isDeleted) return res.sendStatus(400);
     // const cart = await Cart.deleteMany({ customerId: req.params.id });
     // const orders = await Order.deleteMany({ customerId: req.params.id });
-    res.sendStatus(200);
+    res.status(200).send({});
+    // res.sendStatus(200);
   } catch (error) {
     res.sendStatus(500).send(error.message);
   }
