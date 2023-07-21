e886a1826c9ba85e877a2ac500956f6068fc352c
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-21 22:27:12 +0530

    updated cookie parameters

--- server/src/routes/logoutRoute.js
+++ server/src/routes/logoutRoute.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/").put(async (req, res) => {
+router.route("/").delete(async (req, res) => {
   try {
-    res
-      .clearCookie("refreshToken", {
-        httpOnly: true,
-      })
-      .sendStatus(200);
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+    });
+
+    res.end();
   } catch (error) {
     console.log(error.message);
     res.sendStatus(500);
