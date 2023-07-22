05f91005164dfc1b658dbbf43934fc7e77ab6bac
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-22 15:59:52 +0530

    added side effects

--- client/src/pages/RootPage.js
+++ client/src/pages/RootPage.js
@@ -2,14 +2,33 @@ import { Outlet } from "react-router-dom";
 import TopNavBar from "../layouts/TopNavBar";
 import { useEffect, useContext } from "react";
 import AuthContext from "../context/AuthContextProvider";
+import useMakeAuthRequest from "../hooks/useMakeAuthRequest";
+import { useSelector } from "react-redux";
+import { usersGetRequest } from "../redux/usersSlice";
+import { cartGetRequest } from "../redux/cartSlice";
 
 function RootPage() {
   const { refreshAuth, isLoggedIn } = useContext(AuthContext);
+
+  const user = useSelector((state) => state.users);
+  const cart = useSelector((state) => state.cart);
+
+  const makeAuthRequest = useMakeAuthRequest();
+
+  // making token refresh request on app loading
   useEffect(() => {
     if (!isLoggedIn) {
-      makeRefreshRequest();
+      makeRefreshRequest(user, usersGetRequest);
     }
-  }, []);
+  }, [isLoggedIn]);
+
+  // fetching cart and user on login
+  useEffect(() => {
+    if (isLoggedIn) {
+      makeAuthRequest(user, usersGetRequest);
+      makeAuthRequest(cart, cartGetRequest);
+    }
+  }, [isLoggedIn]);
 
   const makeRefreshRequest = async () => {
     await refreshAuth();
