a9364474f0ada7a60c66d24ccbabc3553caf3330
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-26 13:19:46 +0530

    updated page - changed rendering logic - removed side-effects - removed functions

--- client/src/pages/OrdersPage.js
+++ client/src/pages/OrdersPage.js
@@ -1,87 +1,13 @@
-import { useSelector, useDispatch } from "react-redux";
-import { ordersGetRequest } from "../redux/ordersSlice";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import reducerStatus from "../assets/ReducerStatus";
+import ErrorLayout from "../layouts/ErrorLayout";
+import LoadingLayout from "../layouts/LoadingLayout";
+import NoItemsLayout from "../layouts/NoItemsLayout";
 import NoOrdersLayout from "../layouts/NoOrdersLayout";
 import OrderedItemLayout from "../layouts/OrderedItemLayout";
-import reducerStatus from "../assets/ReducerStatus";
-import NoItemsLayout from "../layouts/NoItemsLayout";
-import LoadingLayout from "../layouts/LoadingLayout";
-import ErrorLayout from "../layouts/ErrorLayout";
 
 function OrdersPage() {
-  const selectedUser = useSelector((state) => state.users.users);
-  const orders = useSelector((state) => state.orders.orders);
-
-  const dispatch = useDispatch();
-  const { getAccessTokenSilently, isAuthenticated, isLoading } = useAuth0();
-
-  // GETTING ORDERS ON RENDER
-  useEffect(() => {
-    const getOrders = async () => {
-      try {
-        if (isAuthenticated && !isLoading) {
-          const acessToken = await getAccessTokenSilently({
-            authorizationParams: {
-              audience: process.env.REACT_APP_AUDIENCE,
-              scope: "write:orders",
-            },
-          });
-
-          dispatch(
-            ordersGetRequest({
-              acessToken: acessToken,
-              customerId: selectedUser._id,
-            })
-          );
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getOrders();
-  }, []);
-
-  let filteredProductIds = []; // to hold list of productIds in order
-
-  let processedData = []; // to hold final dataset
-
-  if (orders.length > 0 && typeof orders === "object") {
-    // populating productIds
-    orders.forEach((item) => {
-      item.products.map((productItem) => {
-        filteredProductIds.push(productItem.productId);
-      });
-    });
-  }
-
-  const product = useSelector((state) =>
-    state.products.products.filter((item) =>
-      filteredProductIds.includes(item._id)
-    )
-  );
-
-  // generating final dataset
-  if (orders.length > 0 && typeof orders === "object") {
-    orders.forEach((orderItem) => {
-      const temp = { _id: orderItem._id, createdAt: orderItem.createdAt }; // to hold a single instance of processed data
-      const filteredProducts = [];
-      // generating product details for final dataset (adding product qty from orders to final dataset)
-      orderItem.products.forEach((orderProductItem) => {
-        product.map((productItem) => {
-          if (productItem._id === orderProductItem.productId) {
-            filteredProducts.push({
-              ...productItem,
-              qty: orderProductItem.qty,
-            });
-          }
-        });
-      });
-      temp.products = filteredProducts; // adding processed product data to an instance of processed data
-      processedData.push(temp);
-    });
-  }
+  const orders = useSelector((state) => state.orders);
 
   return (
     <>
@@ -93,7 +19,11 @@ function OrdersPage() {
 
       {(orders.status === reducerStatus.fulfilled) &
       (orders.orders.length > 0) ? (
-        <OrderedItemLayout processedData={processedData} />
+        <>
+          {orders.orders.map((item) => {
+            return <OrderedItemLayout order={item} />;
+          })}
+        </>
       ) : (
         <NoOrdersLayout />
       )}
