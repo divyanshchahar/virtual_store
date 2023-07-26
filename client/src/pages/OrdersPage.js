18534aac33c667f066baac932ada180c0532e73c
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-26 13:28:32 +0530

    added side effects

--- client/src/pages/OrdersPage.js
+++ client/src/pages/OrdersPage.js
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import reducerStatus from "../assets/ReducerStatus";
+import useMakeAuthRequest from "../hooks/useMakeAuthRequest";
 import ErrorLayout from "../layouts/ErrorLayout";
 import LoadingLayout from "../layouts/LoadingLayout";
 import NoItemsLayout from "../layouts/NoItemsLayout";
 import NoOrdersLayout from "../layouts/NoOrdersLayout";
 import OrderedItemLayout from "../layouts/OrderedItemLayout";
+import { ordersGetRequest } from "../redux/ordersSlice";
 
 function OrdersPage() {
   const orders = useSelector((state) => state.orders);
+
+  const makeAuthRequest = useMakeAuthRequest();
+  // fetching orders ono render
+  useEffect(() => {
+    makeAuthRequest(orders, ordersGetRequest);
+  }, [orders]);
 
   return (
     <>
