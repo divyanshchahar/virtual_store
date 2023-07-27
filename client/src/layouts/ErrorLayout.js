fdd8c94666e850e9429b3cfebd369a8825fc6561
Divyansh Chahar <divyanshchahar@outlook.com>
2023-07-27 20:59:30 +0530

    updated layout - changed html for better formatting - changed attributes

--- client/src/layouts/ErrorLayout.js
+++ client/src/layouts/ErrorLayout.js
@@ -5,9 +5,14 @@ function ErrorLayout() {
         className="d-flex justify-content-center align-items-center"
         style={{ height: "100%" }}
       >
-        <i class="bi bi-exclamation-circle" style={{ fontSize: "10rem" }}></i>
+        <i
+          className="bi bi-exclamation-circle"
+          style={{ fontSize: "10rem" }}
+        ></i>
       </div>
-      <p className="text-center fs-1">Oops! Something went Wrong</p>
+      <div className="mx-5">
+        <p className="text-center fs-1">Oops! Something went Wrong</p>
+      </div>
     </>
   );
 }
