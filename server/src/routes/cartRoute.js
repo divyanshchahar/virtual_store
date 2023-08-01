61d03a674918a1a407e729bab46af21756312b52
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-01 09:50:21 +0530

    updated updated input validation logic for post route updated object being created for post route refactored variable  to changed send object for catch block added logic to send empty object on cart deletion

--- server/src/routes/cartRoute.js
+++ server/src/routes/cartRoute.js
@@ -20,33 +20,36 @@ router.route("/").get(authorizationMiddleware, async (req, res) => {
 
 router.route("/").post(authorizationMiddleware, async (req, res) => {
   try {
-    if (!req.body.productId) return res.sendStatus(400);
+    let hasQty = false;
 
+    if (req?.body?.qty || req?.body?.qty === 0) hasQty = true;
+
+    if (!req.body.productId || !hasQty) return res.status(400);
+
+    if (typeof req.body.qty === "undefined" || "null") res.status(400);
     const isProduct = await Product.findById(req.body.productId);
 
     if (!isProduct) return res.sendStatus(404);
 
     const cart = await Cart.create({
-      customerId: null || req.id,
-      products: [
-        { productId: null || req.body.productId, qty: null || req.body.qty },
-      ],
+      customerId: req.id,
+      products: [{ productId: req.body.productId, qty: req.body.qty }],
     });
 
-    res.sendStatus(200);
+    res.status(200).send(cart);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error);
   }
 });
 
 router.route("/").put(authorizationMiddleware, async (req, res) => {
   try {
     let itemNotPresent = true;
-    let isQty = false;
+    let hasQty = false;
 
-    if (req?.body?.qty || req?.body?.qty === 0) isQty = true;
+    if (req?.body?.qty || req?.body?.qty === 0) hasQty = true;
 
-    if (!req.body.productId || !isQty) return res.sendStatus(400);
+    if (!req.body.productId || !hasQty) return res.sendStatus(400);
 
     const isCustomer = await User.findById(req.id);
     const isProduct = await Product.findById(req.body.productId);
@@ -76,14 +79,14 @@ router.route("/").put(authorizationMiddleware, async (req, res) => {
 
     res.status(200).send(updatedCart);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send(error);
   }
 });
 
 router.route("/").delete(authorizationMiddleware, async (req, res) => {
   try {
     const { isDeleted } = await Cart.deleteOne({ customerId: req.id });
-    if (isDeleted.deletecount > 0) return res.status(200);
+    if (isDeleted.deletecount > 0) return res.status(200).send({});
     return res.status(404);
   } catch (error) {
     return res.send(error).status(500);
