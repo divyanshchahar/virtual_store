7d1b96aa8ea094f950f003ccbebab27927758173
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-01 09:52:41 +0530

    changed  to

--- server/src/routes/loginRoute.js
+++ server/src/routes/loginRoute.js
@@ -44,7 +44,7 @@ router.route("/").post(async (req, res) => {
         .status(200);
     }
 
-    res.sendStatus(401);
+    res.status(401);
   } catch (error) {
     res.status(500).send(error.message);
   }
