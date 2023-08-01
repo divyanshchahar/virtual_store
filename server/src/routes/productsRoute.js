a7653597790018b2f8397dada0e847d7713bce5b
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-01 10:09:04 +0530

    updated  block

--- server/src/routes/productsRoute.js
+++ server/src/routes/productsRoute.js
@@ -7,8 +7,8 @@ router.route("/").get(async (req, res) => {
   try {
     const products = await Products.find();
     res.status(200).send(products);
-  } catch (e) {
-    res.status(500).send(e.message);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
@@ -16,8 +16,8 @@ router.route("/:productId").get(async (req, res) => {
   try {
     const result = await Products.findById(req.params.productId);
     res.status(200).json(result);
-  } catch (e) {
-    res.status(500).send(e.message);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
