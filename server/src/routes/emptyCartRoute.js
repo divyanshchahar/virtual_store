85d84c8e71a856cb1badc9725c1ef73f9b918182
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-02 14:43:13 +0530

    added empty cart route

---
+++ server/src/routes/emptyCartRoute.js
@@ -0,0 +1,23 @@
+const Cart = require("../schema/cartSchema");
+const express = require("express");
+const authorizationMiddleware = require("../middleware/authorizationMiddleware");
+
+const router = express.Router;
+
+router.route("/").put(authorizationMiddleware, async (req, res) => {
+  try {
+    const [cart] = await Cart.find({ customerId: req.id });
+
+    if (!cart) return res.status(404).end();
+
+    cart.products = {};
+
+    const updatedCart = await cart.save();
+
+    res.status(200).send(updatedCart).end();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
