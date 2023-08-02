80254905603a0aa2e294fb165e566d9fc74b7384
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-02 15:07:10 +0530

    minor changes - added .end to response objects - replaced json with send

--- server/src/routes/productsRoute.js
+++ server/src/routes/productsRoute.js
@@ -6,18 +6,18 @@ const router = express.Router();
 router.route("/").get(async (req, res) => {
   try {
     const products = await Products.find();
-    res.status(200).send(products);
+    res.status(200).send(products).end();
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error).end();
   }
 });
 
 router.route("/:productId").get(async (req, res) => {
   try {
     const result = await Products.findById(req.params.productId);
-    res.status(200).json(result);
+    res.status(200).send(result).end();
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error).end();
   }
 });
 
