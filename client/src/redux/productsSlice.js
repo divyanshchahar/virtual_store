973ab59cca5e38dea67e17e1e16ba0ad08e4b910
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-02 19:30:41 +0530

    refactored products reducer

--- client/src/redux/productsSlice.js
+++ client/src/redux/productsSlice.js
@@ -5,6 +5,7 @@ import reducerStatus from "../assets/ReducerStatus";
 const initialState = {
   products: [],
   status: "idle",
+  response: null,
   error: null,
 };
 
@@ -15,9 +16,9 @@ export const getProductsApi = createAsyncThunk(
     try {
       const response = await fetch(apiEndPoints.products);
       const json = await response.json();
-      return { ok: response.ok, body: json };
+      return { ok: response.ok, body: json.user, response: response.status };
     } catch (error) {
-      return error.message;
+      return { ok: false, body: {}, response: 500, error: error };
     }
   }
 );
@@ -30,24 +31,28 @@ const productsSlice = createSlice({
     builder
       .addCase(getProductsApi.pending, (state, action) => {
         state.status = reducerStatus.pending;
-        state.products = [];
+        state.products = {};
         state.error = null;
+        state.response = null;
       })
       .addCase(getProductsApi.fulfilled, (state, action) => {
         if (!action.payload.ok || typeof action.payload.body !== "object") {
           state.status = reducerStatus.rejected;
-          state.products = [];
-          state.error = null || action.payload?.body;
+          state.products = {};
+          state.error = null;
+          state.response = action.payload.response;
         } else {
           state.status = reducerStatus.fulfilled;
           state.products = action.payload.body;
           state.error = null;
+          state.response = action.payload.response;
         }
       })
       .addCase(getProductsApi.rejected, (state, action) => {
         state.status = reducerStatus.rejected;
-        state.products = [];
-        state.error = action.payload;
+        state.users = {};
+        state.error = null || action.payload;
+        state.response = action.payload.response;
       });
   },
 });
