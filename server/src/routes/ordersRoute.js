778d8306d1326ad2c65a5bbc5f8403982f0b44a3
Divyansh Chahar <divyanshchahar@outlook.com>
2023-08-02 15:02:52 +0530

    monir changes - added .end to response objects

--- server/src/routes/ordersRoute.js
+++ server/src/routes/ordersRoute.js
@@ -8,20 +8,20 @@ router.route("/").get(authorizationMiddleware, async (req, res) => {
   try {
     const orders = await Orders.find({ customerId: req.id });
 
-    if (!orders) return res.status(404);
+    if (!orders) return res.status(404).end();
 
-    res.status(200).send(orders);
+    res.status(200).send(orders).end();
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error).end();
   }
 });
 
 router.route("/").post(authorizationMiddleware, async (req, res) => {
   try {
     const orders = await Orders.create({ customerId: req.id, ...req.body });
-    res.status(200).send(orders);
+    res.status(200).send(orders).end();
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error).end();
   }
 });
 
